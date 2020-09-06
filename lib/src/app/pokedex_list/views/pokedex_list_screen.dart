82f0ed42a2b00bf4f28e6ccbac470e65b0ae2c95
Anderson Menezes <a.menezes@coblue.com.br>
2020-09-05 21:17:39 -0300

    card do pokemon basico

--- lib/src/app/pokedex_list/views/pokedex_list_screen.dart
+++ lib/src/app/pokedex_list/views/pokedex_list_screen.dart
@@ -4,72 +4,137 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_pokeapi/src/models/pokemon.dart';
+import 'package:flutter_pokeapi/src/utils/type_to_color_mapper.dart';
 
 class PokedexListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
 
-    return FutureBuilder(
-      future: rootBundle.loadString('lib/fakeData/fakeApi.json'),
-      builder: (context, snapshot) {
-        List<Pokemon> list = [];
-        if (snapshot.hasData) {
-          final jsonList =
-              json.decode(snapshot.data)['pokemon'] as List<dynamic>;
-          jsonList.forEach((element) {
-            // print(element['weaknesses']);
-            final pokemon = Pokemon.fromMap(element);
-            list.add(pokemon);
-          });
-        }
-        // for (final pokemon in jsonList) {
-        //   print(pokemon);
-        //   print(Pokemon.fromMap(pokemon));
-        // }
-        return Scaffold(
-          body: Container(
-            child: Stack(
-              children: [
-                Positioned(
-                  bottom: 15,
-                  right: 15,
-                  child: Opacity(
-                    opacity: 0.15,
-                    child: Image.asset(
-                      'lib/assets/pokebola.png',
-                      width: mediaQuery.size.width * 0.5,
-                    ),
-                  ),
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 15,
+              right: 15,
+              child: Opacity(
+                opacity: 0.15,
+                child: Image.asset(
+                  'lib/assets/pokebola.png',
+                  width: mediaQuery.size.width * 0.5,
                 ),
-                if (snapshot.hasData)
-                  CustomScrollView(
-                    slivers: [
-                      SliverAppBar(
-                        floating: true,
-                        pinned: true,
-                        flexibleSpace: SafeArea(
-                          child: Opacity(
-                              opacity: 0.2,
-                              child:
-                                  Image.asset('lib/assets/pokemon-logo.png')),
-                        ),
-                        expandedHeight: 200,
-                      ),
-                      SliverList(
-                        delegate: SliverChildBuilderDelegate(
-                          (context, index) => ListTile(
-                              title: Text('Pokemon ${list[index].name}')),
-                          childCount: list.length,
-                        ),
-                      )
-                    ],
+              ),
+            ),
+            CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  floating: true,
+                  pinned: true,
+                  collapsedHeight: 60,
+                  toolbarHeight: 49,
+                  flexibleSpace: SafeArea(
+                    child: Image.asset('lib/assets/pokemon-logo.png'),
                   ),
+                  expandedHeight: 200,
+                ),
+                FutureBuilder(
+                  future: rootBundle.loadString('lib/fakeData/fakeApi.json'),
+                  builder: (context, snapshot) {
+                    List<Pokemon> list = [];
+                    if (snapshot.hasData) {
+                      final jsonList = json.decode(snapshot.data)['pokemon']
+                          as List<dynamic>;
+                      jsonList.forEach(
+                        (element) {
+                          // print(element['weaknesses']);
+                          final pokemon = Pokemon.fromMap(element);
+                          list.add(pokemon);
+                        },
+                      );
+
+                      return SliverPadding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        sliver: SliverGrid(
+                          gridDelegate:
+                              SliverGridDelegateWithMaxCrossAxisExtent(
+                                  crossAxisSpacing: 15,
+                                  mainAxisSpacing: 10,
+                                  childAspectRatio: 0.95,
+                                  maxCrossAxisExtent:
+                                      mediaQuery.size.height * 0.5),
+                          delegate: SliverChildBuilderDelegate(
+                            (context, index) {
+                              final pokemon = list.elementAt(index);
+
+                              return GridTile(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child: Container(
+                                    color: TypeToColorMapper
+                                        .colorMapper[pokemon.type[0]],
+                                    child: Stack(
+                                      children: [
+                                        Positioned(
+                                          child: Text('${pokemon.name}'),
+                                          left: 15,
+                                          top: 10,
+                                        ),
+                                        Positioned(
+                                            width: 350,
+                                            child: Opacity(
+                                              opacity: 0.05,
+                                              child: ColorFiltered(
+                                                colorFilter: ColorFilter.mode(
+                                                    Colors.white,
+                                                    BlendMode.color),
+                                                child: Image.asset(
+                                                    'lib/assets/pokeball-transparent.png'),
+                                              ),
+                                            )),
+                                        Positioned(
+                                          bottom: -10,
+                                          right: -10,
+                                          child: Image.network(
+                                            "${pokemon.img}",
+                                            scale: 0.9,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                            childCount: list.length,
+                          ),
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      return SliverToBoxAdapter(
+                        child: Container(
+                          child: Text('erro'),
+                        ),
+                      );
+                    } else {
+                      return SliverToBoxAdapter(
+                        child: Container(
+                          width: mediaQuery.size.width,
+                          height: mediaQuery.size.height,
+                          child: Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                )
               ],
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/src/utils/type_to_color_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TypeToColorMapper {
+  static Map<String, Color> colorMapper = {
+    'Grass': Colors.green[400],
+    'Fire': Colors.red[400],
+    'Water': Colors.blue[300],
+    'Bug': Colors.purple[200],
+    'Electric': Colors.yellow[400],
+    'Ground': Colors.brown[800],
+    'Poison': Colors.purple[700],
+    'Normal': Colors.teal[100],
+    'Psychic': Colors.grey[300],
+    'Fighting': Colors.brown[400],
+    'Rock': Colors.grey[600],
+    'Ghost': Colors.blueGrey[600],
+    'Ice': Colors.blue[100],
+    'Dragon': Colors.deepOrange
+  };
+}
