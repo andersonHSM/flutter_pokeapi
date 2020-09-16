805b6e897fb169c85e6e1ea30789b27afee7f4dd
Anderson Menezes <a.menezes@coblue.com.br>
2020-09-16 09:23:40 -0300

    atualizaçao de dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pokeapi/src/app/pokemon_details/views/pokemon_details_screen.dart';
-import 'package:flutter_pokeapi/src/utils/app_routes.dart';
-import 'package:flutter_pokeapi/src/widgets/home_screen.dart';
+import 'package:flutter_pokeapi/src/app/app.dart';
 
 void main() {
   runApp(PokedexApp());
-}
-
-class PokedexApp extends StatelessWidget {
-  final isLoggedIn = false;
-  Widget build(context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        accentColor: Colors.yellow[600],
-      ),
-      routes: {
-        AppRoutes.HOME: (ctx) => HomeScreen(),
-        AppRoutes.POKEMON_DETAILS: (ctx) => PokemonDetailsScreen()
-      },
-    );
-  }
 }
